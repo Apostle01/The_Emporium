ed0775c4265903416fe3739384c674577233cb08
Jewel <nanaafianyamekye2@gmail.com>
2024-11-08 03:51:02 +0000

    Updating shopping cart page added

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-
+from haven.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -29,4 +29,14 @@ class Cart():
 
     def __len__(self):
         return len(self.cart)
+    
+    def get_prods(self):
+        # Get ids from cart
+        product_ids = self.cart.keys()
+        # Use ids to lookup products in database model
+        products = Product.objects.filter(id__in=product_ids)
+
+        # Return those looked up products
+        return products
+
         
