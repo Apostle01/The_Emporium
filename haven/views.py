1cf75c7dd1ce43446196c0eeb9b8b14e7204d684
Jewel <nanaafianyamekye2@gmail.com>
2024-11-13 22:34:20 +0000

    Added search product function

--- haven/views.py
+++ haven/views.py
@@ -1,77 +1,99 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category, Profile, ShippingAddress
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm, ShippingAddressForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+
+# from payment.forms import ShippingForm
+# from payment.models import ShippingAddress
+
 from django import forms
-# from .models import ShippingAddress, Profile
+from django.db.models import Q
+import json
+from cart.cart import Cart
+
+
+def search(request):
+	# Determine if they filled out the form
+	if request.method == "POST":
+		searched = request.POST.get('searched', '')
+		# Query The Products DB Model
+		searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+		# Test for null
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+		else:
+			return render(request, "search.html", {'searched':searched})
+	else:
+		return render(request, "search.html", {})	
 
 
 def update_info(request):
+	if request.user.is_authenticated:
+		# Get Current User
+		current_user = Profile.objects.get(user__id=request.user.id)
+		# Get Current User's Shipping Info
+		shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		
+		# Get original User Form
+		form = UserInfoForm(request.POST or None, instance=current_user)
+		# Get User's Shipping Form
+		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
+		if form.is_valid() or shipping_form.is_valid():
+			# Save original form
+			form.save()
+			# Save shipping form
+			shipping_form.save()
 
-    if request.user.is_authenticated:
-    
-        # Get or create the Profile and ShippingAddress instances
-        current_user = Profile.objects.get(user__id=request.user.id)
-        # Try to get the user's shipping address, create it if it doesn’t exist
-        shipping_user, created = ShippingAddress.objects.get_or_create(user=request.user)
-        # Initialize the forms
-        form = UserInfoForm(request.POST or None, instance=current_user)
-        shipping_form = ShippingAddressForm(request.POST or None, instance=shipping_user)
+			messages.success(request, "Your Info Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
 
-        if request.method == 'POST':
-            if form.is_valid() and shipping_form.is_valid():
-                form.save()
-                shipping_form.save()
-                messages.success(request, "Your info has been updated!")
-                return redirect('home')
 
-        return render(request, "update_info.html", {'form': form, 'shipping_form': shipping_form})
-    else:
-        messages.error(request, "You must be logged in to access this page.")
-        return redirect('home')
-	
 
 def update_password(request):
-    if request.user.is_authenticated:
-        current_user = request.user
-        # Did they fill out the form
-        if request.method == 'POST':
-            form = ChangePasswordForm(current_user, request.POST)
-            # Is the form valid
-            if form.is_valid():
-                form.save()
-                messages.success(request, "Your Password Has Been Updated...")
-                login(request, current_user)
-                return redirect('update_user')
-            else:
-                for error in list(form.errors.values()):
-                    messages.error(request, error)
-                return redirect('update_password')
-        else:
-            form = ChangePasswordForm(current_user)
-            return render(request, "update_password.html", {'form': form})
-    else:
-        messages.success(request, "You Must Be Logged In To View That Page...")
-        return redirect('home')
-
+	if request.user.is_authenticated:
+		current_user = request.user
+		# Did they fill out the form
+		if request.method  == 'POST':
+			form = ChangePasswordForm(current_user, request.POST)
+			# Is the form valid
+			if form.is_valid():
+				form.save()
+				messages.success(request, "Your Password Has Been Updated...")
+				login(request, current_user)
+				return redirect('update_user')
+			else:
+				for error in list(form.errors.values()):
+					messages.error(request, error)
+					return redirect('update_password')
+		else:
+			form = ChangePasswordForm(current_user)
+			return render(request, "update_password.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
 def update_user(request):
-    if request.user.is_authenticated:
-        current_user = User.objects.get(id=request.user.id)
-        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		user_form = UpdateUserForm(request.POST or None, instance=current_user)
 
-        if user_form.is_valid():
-            user_form.save()
-            login(request, current_user)
-            messages.success(request, "User Has Been Updated!!")
-            return redirect('home')
-        return render(request, "update_user.html", {'user_form': user_form})
-    else:
-        messages.success(request, "You Must Be Logged In To Access That Page!!")
-        return redirect('home')
+		if user_form.is_valid():
+			user_form.save()
 
+			login(request, current_user)
+			messages.success(request, "User Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_user.html", {'user_form':user_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
 
 
 def category_summary(request):
