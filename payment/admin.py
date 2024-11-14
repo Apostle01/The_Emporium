063c8e67b5b318343759db4fc120a78705a2797f
Jewel <nanaafianyamekye2@gmail.com>
2024-11-14 20:07:41 +0000

    Added Customer Order Models

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, OrderItem, Order
 
 
 # Register the model on the admin section thing
 admin.site.register(ShippingAddress)
+admin.site.register(OrderItem)
+admin.site.register(Order)
 
 
