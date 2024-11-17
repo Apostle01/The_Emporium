779e3e87d81ba84212bd63e948064e9f29ca109e
Jewel <nanaafianyamekye2@gmail.com>
2024-11-17 13:01:32 +0000

    Shipping Dashboard added for shipped and unshipped items

--- payment/urls.py
+++ payment/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('billing_info/', views.billing_info, name='billing_info'),
     path('update_shipping/', views.update_shipping, name='update_shipping'),
     path('process_order/', views.process_order, name='process_order'),
+    path('shipped_dash/', views.shipped_dash, name='shipped_dash'),
+    path('not_shipped_dash/', views.not_shipped_dash, name='not_shipped_dash'),
 ]
