63fba7ed8fb7b646f1419283cab08a1b4b011db1
Jewel <nanaafianyamekye2@gmail.com>
2024-11-18 21:40:02 +0000

    modified:   db.sqlite3         modified:   haven/migrations/0001_initial.py         deleted:    haven/migrations/0002_alter_category_options_product_is_sale_and_more.py         deleted:    haven/migrations/0003_profile.py         deleted:    haven/migrations/0004_profile_old_cart.py         deleted:    haven/migrations/0005_shippingaddress.py         deleted:    haven/migrations/0006_alter_profile_old_cart.py         deleted:    haven/migrations/0007_alter_profile_old_cart.py         modified:   haven/migrations/__pycache__/0001_initial.cpython-312.pyc         modified:   payment/migrations/0001_initial.py         deleted:    payment/migrations/0002_rename_address1_shippingaddress_shipping_address1_and_more.py         deleted:    payment/migrations/0003_alter_shippingaddress_shipping_address2_order_and_more.py         deleted:    payment/migrations/0004_order_shipped.py         deleted:    payment/migrations/0005_order_date_shipped.py         modified:   payment/migrations/__pycache__/0001_initial.cpython-312.pyc         modified:   the_emporium/__pycache__/settings.cpython-312.pyc         modified:   the_emporium/settings.py

--- the_emporium/settings.py
+++ the_emporium/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+#Load our environmental variables
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,10 +84,15 @@ WSGI_APPLICATION = 'the_emporium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',  # Your database name
+        'USER': 'postgres',  # Your PostgreSQL username
+        'PASSWORD': os.environ.get('DB_PASSWORD'),  # Correct
+        'HOST': '127.0.0.1',  # Your database host
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
